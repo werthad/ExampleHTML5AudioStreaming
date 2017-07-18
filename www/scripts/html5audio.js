1f84312fb86b6045fc4c8c73d0fc533ffb23f5d0
SKT <werthad@gmail.com>
2017-07-18 22:34:53 +0200

    Update html5audio.js

--- www/scripts/html5audio.js
+++ www/scripts/html5audio.js
@@ -22,7 +22,7 @@ function pad2(number) {
 	return (number < 10 ? '0' : '') + number
 }
 
-var myaudioURL = 'http://92.61.114.195:9960;';
+var myaudioURL = 'http://92.61.114.195:9960/;';
 var myaudio = new Audio(myaudioURL);
 var isPlaying = false;
 var readyStateInterval = null;
